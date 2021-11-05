b6bf83f827c40a0298cdecb1e89e5d02a19d0049
Daniil Karpovych <kadavi444@gmail.com>
2021-11-05 16:22:37 +0200

    fix test

--- client/src/component/_tests_/Settings.test.tsx
+++ client/src/component/_tests_/Settings.test.tsx
@@ -4,7 +4,7 @@ import { render } from './test-utils';
 
 test('renders learn react link', () => {
   const store = render(<Setting />, { preloadedState: { socket: {}, selectedCompany: { selectedCompany: ["test company"]} }});
-  const linkElement = screen.getByText("Reset");
+  const linkElement = screen.getByText("Cancel Delete");
   linkElement.click();
   expect(store.getState().selectedCompany.selectedCompany).toStrictEqual([]);
 });
